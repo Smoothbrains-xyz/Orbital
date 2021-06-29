8bd22d485515c7acc81ee9dc886e6ec17149caa2
Abheek Dhawan <abheekd@protonmail.com>
2021-06-29 17:21:48 +0100

    Update iss embed color

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ async function iss(interaction) {
         .setTitle("The current location of the ISS!")
         .setURL('https://spotthestation.nasa.gov/tracking_map.cfm')
         .setImage(`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/pin-s+000(${data.iss_position.longitude},${data.iss_position.latitude})/-87.0186,20,1/1000x1000?access_token=pk.eyJ1IjoiYWRhd2Vzb21lZ3V5IiwiYSI6ImNrbGpuaWdrYzJ0bGYydXBja2xsNmd2YTcifQ.Ude0UFOf9lFcQ-3BANWY5A`)
-        .setColor("00c5ff")
+        .setColor("ffffff")
         .setFooter(`Bot ID: ${client.user.id}`)
         .setTimestamp();
       axios.get("http://api.open-notify.org/astros.json")
