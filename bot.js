eb6fc1f0117106f42a410d9fc0396d660cb5982b
Abheek Dhawan <abheekd@protonmail.com>
2021-06-28 22:18:28 -0500

    Define initial intents

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS"] });
 
 const axios = require('axios');
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
 
 client.login(process.env.TOKEN);
