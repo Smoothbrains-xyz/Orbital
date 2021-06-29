908b93166da1a42dcced341139ad0693e8ed81fc
Abheek Dhawan <abheekd@protonmail.com>
2021-06-28 22:42:26 -0500

    Add initial bot code with NASA's Astronomy Picture of the Day

--- bot.js
+++ bot.js
@@ -1,12 +1,49 @@
+/* ENV VARIABLES
+*  TOKEN: Bot token
+*  NASA_API_KEY: NASA API Key
+*/
+
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS"] });
 
 const axios = require('axios');
 
+const urls = require('./config/urls.json');
+
 client.once('ready', () => {
+  // Log bot tag to console on start
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on("interaction", interaction => {
+  // If the interaction isn't a slash command, return
+  if (!interaction.isCommand()) return;
 
+  switch(interaction.commandName) {
+    case "apod":
+      apod(interaction);
+      break;
+  } // End interaction command name switch
+});
+
+async function apod(interaction) {
+  const nasaApiKey = process.env.NASA_API_KEY;
+  axios.get(`${urls.apod}${nasaApiKey}`)
+    .then(response => {
+      const apodEmbed = new Discord.MessageEmbed()
+        .setAuthor(interaction.user.tag, interaction.author.displayAvatarURL({ dynamic: true, size: 1024 }))
+        .setTitle(response.title)
+        .setDescription(response.explanation)
+        .setField('Copyright', `©️ ${response.copyright}`, true)
+        .setField('Link', `[Click here!](response.hdurl)`, true)
+        .setImage(response.hdurl)
+        .setFooter(`Bot ID: ${client.user.id}`)
+        .setColor('ffffff')
+        .setTimestamp();
+
+      interaction.reply(apodEmbed);
+    })
+    .catch(console.error);
+}
 
 client.login(process.env.TOKEN);
