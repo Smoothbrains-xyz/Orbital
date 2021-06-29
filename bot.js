b8937b67fbe08d602e5c61091fb55525519954c6
Navin Narayanan <nan.navinnarayanan@gmail.com>
2021-06-29 16:37:02 -0500

    Merge Fix

--- bot.js
+++ bot.js
@@ -67,6 +67,11 @@ async function iss(interaction) {
         .setAuthor(interaction.user.tag, interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }))
         .setTitle("International Space Station")
         .setImage(`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/pin-s+000(${data.iss_position.longitude},${data.iss_position.latitude})/-87.0186,20,1/1000x1000?access_token=pk.eyJ1IjoiYWRhd2Vzb21lZ3V5IiwiYSI6ImNrbGpuaWdrYzJ0bGYydXBja2xsNmd2YTcifQ.Ude0UFOf9lFcQ-3BANWY5A`)
+<<<<<<< Updated upstream
+=======
+        .setFooter(embedInfo.footer[0], embedInfo.footer[1])
+        .setColor("ffffff")
+>>>>>>> Stashed changes
         .setFooter(`Bot ID: ${client.user.id}`)
         .setColor("ffffff")
         .setTimestamp();
@@ -82,6 +87,32 @@ async function iss(interaction) {
     .catch(console.error);
 }
 
+<<<<<<< Updated upstream
+=======
+async function help(interaction) {
+      const helpEmbed = new Discord.MessageEmbed()
+        .setTitle("Help Command • Orbital")
+        .setDescription("TODO")
+        .addField("Command List:", "TODO")
+        .setColor("ffffff")
+        .setFooter(embedInfo.footer[0], embedInfo.footer[1])
+        .setTimestamp();
+
+      interaction.reply({ embeds: [helpEmbed] });
+}
+
+async function ping(interaction) {
+  const pingEmbed = new Discord.MessageEmbed()
+    .setTitle("Current Ping • Orbital")
+    .setDescription(`:ping_pong: Pong: ${client.ws.ping}ms!`)
+    .setColor("ffffff")
+    .setFooter(embedInfo.footer[0], embedInfo.footer[1])
+    .setTimestamp();
+
+  interaction.reply({ embeds: [pingEmbed] });
+}
+
+>>>>>>> Stashed changes
 async function epic (action, interaction) {
   if (action === "natural") {
     axios.get(`${urls.epic_natural_date}${nasaApiKey}`)
