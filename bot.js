b3078fd7035f25cf5b9aba25aea0df11ec36ff4b
Abheek Dhawan <abheekd@protonmail.com>
2021-06-29 12:35:34 -0500

    Create function for EPIC

--- bot.js
+++ bot.js
@@ -30,6 +30,9 @@ client.on("interaction", interaction => {
     case "iss":
       iss(interaction);
       break;
+    case "epic":
+      epic(interaction.options[0].name /* Subcommand name */, interaction);
+      break;
   } // End interaction command name switch
 });
 
@@ -75,6 +78,14 @@ async function iss(interaction) {
         });
     })
     .catch(console.error);
+}
+
+async function epic (action, interaction) {
+  if (action === "natural") {
+    console.log(interaction);
+  } else if (action === "enhanced") {
+    console.log(interaction);
+  }
 }
 
 client.login(process.env.TOKEN);
