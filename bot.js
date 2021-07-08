6acddeb13810fb1e0b86c5e27140c07f4896d2c9
Abheek Dhawan <abheekd@protonmail.com>
2021-07-08 12:19:12 -0500

    Require gitlog

--- bot.js
+++ bot.js
@@ -12,6 +12,7 @@ const client = new Discord.Client({
 
 const axios = require('axios');
 const parseString = require('xml2js').parseString;
+const gitlog = require("gitlog").default;
 require('dotenv').config();
 
 const urls = require('./config/urls.json');
